0a46f129a8e91f28481921027226c783a4f0a98f
thanhdat <kimthanhdat0510@gmail.com>
2023-05-28 14:36:30 +0700

    re admin account

--- src/container/Header.jsx
+++ src/container/Header.jsx
@@ -134,26 +134,24 @@ const Header = () => {
                 exit={{ opacity: 0, scale: 0.6 }}
                 className='w-44 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0'
               >
-                {(user && user.email === 'ktd1302@gmail.com') ||
-                  ('ktd11@gmail.com' && (
-                    <>
-                      <Link to={'/dashboard/home'}>
-                        <p
-                          className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 hover:rounded-t-lg transition-all duration-100 ease-in-out text-textColor text-base'
-                          onClick={() => setIsMenu(false)}
-                        >
-                          <MdOutlineDashboardCustomize /> Trang quản trị
-                        </p>
-                      </Link>
+                {user && user.email === 'ktd11@gmail.com' && (
+                  <>
+                    <Link to={'/dashboard/home'}>
                       <p
-                        className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100  transition-all duration-100 ease-in-out text-textColor text-base'
-                        onClick={() => setShowProfile(true)}
+                        className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 hover:rounded-t-lg transition-all duration-100 ease-in-out text-textColor text-base'
+                        onClick={() => setIsMenu(false)}
                       >
-                        <CgProfile /> Hồ sơ
+                        <MdOutlineDashboardCustomize /> Trang quản trị
                       </p>
-                    </>
-                  ))}
-
+                    </Link>
+                  </>
+                )}
+                <p
+                  className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100  transition-all duration-100 ease-in-out text-textColor text-base'
+                  onClick={() => setShowProfile(true)}
+                >
+                  <CgProfile /> Hồ sơ
+                </p>
                 <p
                   className='m-2 p-2 rounded-md shadow-md flex items-center justify-center bg-gray-200 gap-3 cursor-pointer hover:bg-gray-300 transition-all duration-100 ease-in-out text-textColor text-base'
                   onClick={logout}
@@ -316,19 +314,18 @@ const Header = () => {
                       <CgProfile /> Hồ sơ
                     </p>
                   </ul>
-                  {(user && user.email === 'ktd1302@gmail.com') ||
-                    ('ktd11@gmail.com' && (
-                      <>
-                        <NavLink to={'/dashboard/home'}>
-                          <p
-                            className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 hover:rounded-t-lg transition-all duration-100 ease-in-out text-textColor text-base'
-                            onClick={() => setIsMenu(false)}
-                          >
-                            <MdOutlineDashboardCustomize /> Trang quản trị
-                          </p>
-                        </NavLink>
-                      </>
-                    ))}
+                  {user && user.email === 'ktd11@gmail.com' && (
+                    <>
+                      <NavLink to={'/dashboard/home'}>
+                        <p
+                          className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 hover:rounded-t-lg transition-all duration-100 ease-in-out text-textColor text-base'
+                          onClick={() => setIsMenu(false)}
+                        >
+                          <MdOutlineDashboardCustomize /> Trang quản trị
+                        </p>
+                      </NavLink>
+                    </>
+                  )}
                   <p
                     className='m-2 p-2 rounded-md shadow-md flex items-center justify-center bg-gray-200 gap-3 cursor-pointer hover:bg-gray-300 transition-all duration-100 ease-in-out text-textColor text-base'
                     onClick={logout}
